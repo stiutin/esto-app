784ffb1a64040dc0df049e1244e251c723d5fd7c
Serge <sty@binotel.com>
2025-12-10 12:17:54 +0100

    update NG version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -9,21 +9,14 @@ import { FavoritesComponent } from './favorites/favorites.component';
 import { PhotoDetailsComponent } from './photos/photo-details/photo-details.component';
 import {RouterModule} from '@angular/router';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    PhotosComponent,
-    FavoritesComponent,
-    PhotoDetailsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    RouterModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HeaderComponent,
+        PhotosComponent,
+        FavoritesComponent,
+        PhotoDetailsComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        RouterModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
