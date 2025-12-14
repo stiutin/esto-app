61f90dcf2fa10e2bab7e260ae0e73329285fa9d1
Serge <sty@binotel.com>
2025-12-14 22:34:20 +0100

    added footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "./shared/components/header/header.component";
 import {RouterOutlet} from "@angular/router";
+import {FooterComponent} from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet]
+  imports: [HeaderComponent, RouterOutlet, FooterComponent]
 })
 export class AppComponent {}
